8e31ac8b4eb84bea0cd1fd11b201c231ab73a358
guilleeeee5 <guillermo.ramirez@MacBook-Pro-83.local>
2022-12-21 14:45:33 +0100

    Funcion LeerJson Creada

--- rest-full-service/src/main/java/com/ufvp2g6/restfullservice/LeerJson.java
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/LeerJson.java
@@ -1,9 +1,44 @@
 package com.ufvp2g6.restfullservice;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LeerJson {
     public static ArrayList<ZonaBasicaSalud> LeerFicheroJson1(){
-        
+        ArrayList <ZonaBasicaSalud> listaJSON = new ArrayList<>();
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+
+            // create a reader
+            Reader reader = null;
+            try {
+                reader = Files.newBufferedReader(Paths.get("Covid19-TIA_ZonasBásicasSalud.json"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // convert JSON array to list of users
+            listaJSON = new Gson().fromJson(reader, new TypeToken<List<ZonaBasicaSalud>>() {}.getType());
+            System.out.println("FICHERO JSON LEIDO CORRECTAMENTE");
+
+            // close reader
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return listaJSON;
     }
 }
--- rest-full-service/src/main/java/com/ufvp2g6/restfullservice/RestFullServiceApplication.java
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/RestFullServiceApplication.java
@@ -3,11 +3,16 @@ package com.ufvp2g6.restfullservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class RestFullServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestFullServiceApplication.class, args);
 	}
+	ArrayList<ZonaBasicaSalud> ListaZBS = new ArrayList<>();
+	ArrayList<ZonaBasicaSalud> ListaZBS60 = new ArrayList<>();
 
+	
 }
