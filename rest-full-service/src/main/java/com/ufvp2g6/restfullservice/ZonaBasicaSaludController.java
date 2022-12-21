7f6f11381bea3fda0c0a7e36fdb52869db4acf04
guilleeeee5 <guillermo.ramirez@MacBook-Pro-83.local>
2022-12-21 19:02:59 +0100

    Creado un metodo get que devuelve todas las zonas basicas de salud

--- rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludController.java
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludController.java
@@ -1,8 +1,25 @@
 package com.ufvp2g6.restfullservice;
 
+import com.google.gson.stream.JsonReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ZonaBasicaSaludController {
-    
+
+    @GetMapping("/ZonaBasicaSalud")
+    public ArrayList<ZonaBasicaSalud> zonas(){
+        LeerJson reader = new LeerJson();
+        ArrayList<ZonaBasicaSalud> lista = reader.LeerFicheroJson1();
+        return lista;
+    }
+
+
 }
