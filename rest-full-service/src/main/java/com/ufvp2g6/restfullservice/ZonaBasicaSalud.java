5840df8011dc9c2d6a5cb35b4031139c09b6a402
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2022-12-21 13:30:14 +0100

    Creados los getter y setter del objeto ZonaBasicaSalud

--- rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSalud.java
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSalud.java
@@ -24,5 +24,51 @@ public class ZonaBasicaSalud
     {
     }
 
-    
+    public void setCodigo_geometria(String codigo_geometria) {
+        this.codigo_geometria = codigo_geometria;
+    }
+
+    public void setZona_basica_salud(String zona_basica_salud) {
+        this.zona_basica_salud = zona_basica_salud;
+    }
+
+    public void setTasa_incidencia_acumulada_ultimos_14dias(float tasa_incidencia_acumulada_ultimos_14dias) {
+        this.tasa_incidencia_acumulada_ultimos_14dias = tasa_incidencia_acumulada_ultimos_14dias;
+    }
+
+    public void setTasa_incidencia_acumulada_total(float tasa_incidencia_acumulada_total) {
+        this.tasa_incidencia_acumulada_total = tasa_incidencia_acumulada_total;
+    }
+
+    public void setCasos_confirmados_totales(int casos_confirmados_totales) {
+        this.casos_confirmados_totales = casos_confirmados_totales;
+    }
+
+    public void setFecha_informe(Date fecha_informe) {
+        this.fecha_informe = fecha_informe;
+    }
+
+    public String getCodigo_geometria() {
+        return codigo_geometria;
+    }
+
+    public String getZona_basica_salud() {
+        return zona_basica_salud;
+    }
+
+    public float getTasa_incidencia_acumulada_ultimos_14dias() {
+        return tasa_incidencia_acumulada_ultimos_14dias;
+    }
+
+    public float getTasa_incidencia_acumulada_total() {
+        return tasa_incidencia_acumulada_total;
+    }
+
+    public int getCasos_confirmados_totales() {
+        return casos_confirmados_totales;
+    }
+
+    public Date getFecha_informe() {
+        return fecha_informe;
+    }
 }
