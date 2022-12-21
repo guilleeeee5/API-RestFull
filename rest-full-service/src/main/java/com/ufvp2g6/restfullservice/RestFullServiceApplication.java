3932ead4f0ad32cf9e7b6d2522989f7db2dfdbe3
guilleeeee5 <guillermo.ramirez@MacBook-Pro-83.local>
2022-12-21 11:43:29 +0100

    Inicializado proyecto Spring

---
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/RestFullServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ufvp2g6.restfullservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullServiceApplication.class, args);
+	}
+
+}
---
+++ rest-full-service/src/test/java/com/ufvp2g6/restfullservice/RestFullServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufvp2g6.restfullservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
