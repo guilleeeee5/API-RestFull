149aa755e8fc23148bf87f321e6d52f0273454fd
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2022-12-21 13:28:52 +0100

    Creados contructores del objeto ZonaBasicaSalud

--- rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSalud.java
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSalud.java
@@ -11,6 +11,18 @@ public class ZonaBasicaSalud
     private int casos_confirmados_totales;
     private Date fecha_informe;
 
+    public ZonaBasicaSalud(String codigo_geometria, String zona_basica_salud, float tasa_incidencia_acumulada_ultimos_14dias, float tasa_incidencia_acumulada_total, int casos_confirmados_totales, Date fecha_informe) {
+        this.codigo_geometria = codigo_geometria;
+        this.zona_basica_salud = zona_basica_salud;
+        this.tasa_incidencia_acumulada_ultimos_14dias = tasa_incidencia_acumulada_ultimos_14dias;
+        this.tasa_incidencia_acumulada_total = tasa_incidencia_acumulada_total;
+        this.casos_confirmados_totales = casos_confirmados_totales;
+        this.fecha_informe = fecha_informe;
+    }
+
+    public ZonaBasicaSalud()
+    {
+    }
 
     
 }
