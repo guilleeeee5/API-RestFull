23129b473fba0af84db7325d0c1f446411a947e6
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2022-12-21 13:26:57 +0100

    Creados atributos del objeto ZonaBasicaSalud

---
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSalud.java
@@ -0,0 +1,16 @@
+package com.ufvp2g6.restfullservice;
+
+import java.util.Date;
+
+public class ZonaBasicaSalud
+{
+    private String codigo_geometria;
+    private String zona_basica_salud;
+    private float tasa_incidencia_acumulada_ultimos_14dias;
+    private float tasa_incidencia_acumulada_total;
+    private int casos_confirmados_totales;
+    private Date fecha_informe;
+
+
+    
+}
