5e2791eb5ed09bf7d63ce373f4f1af0338da6b30
JulianPrieto1323 <9000169@alumnos.ufv.es>
2022-12-24 16:07:00 +0100

    Creada la clase escribir JSON

---
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/EscribirJSON.java
@@ -0,0 +1,17 @@
+package com.ufvp2g6.restfullservice;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class EscribirJSON {
+    
+}
