49614f695871ef9b55278dc8fdab4ce4c95ac5e1
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-01-03 19:18:59 +0100

    Añadidos comentarios explicando el código realizado

--- rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludController.java
+++ rest-full-service/src/main/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludController.java
@@ -15,10 +15,13 @@ import java.util.List;
 @RestController
 public class ZonaBasicaSaludController {
 
+    //Creamos los métodos GET para acceder a los elementos del Json leido
     @GetMapping("/ZonaBasicaSalud")
     public ArrayList<ZonaBasicaSalud> zonas(){
         LeerJson reader = new LeerJson();
+        //Leemos el reason e introducimos los elementos en un arraylist
         ArrayList<ZonaBasicaSalud> lista = reader.LeerFicheroJson1();
+        //Mostramos los elementos leidos
         return lista;
     }
 
@@ -36,18 +39,26 @@ public class ZonaBasicaSaludController {
 
            return new ResponseEntity<>(zonaBasicaSalud, HttpStatus.OK);
     }*/
+
+    //Creamos los métodos GET para acceder a los elementos del JSON en base al elemento codigo_geometria
     @GetMapping("/ZonaBasicaSaludMayores60/{codigo_geometria}")
     public ResponseEntity<ZonaBasicaSaludMayores60> getByIDmayores60(@PathVariable String codigo_geometria){
+        //Creamos la clase que nos permita operar los elementos del Json
         DataHanding dataHanding = new DataHanding();
         ZonaBasicaSaludMayores60 zonaBasicaSaludMayores60 = dataHanding.getZBSM60info(codigo_geometria);
         return new ResponseEntity<>(zonaBasicaSaludMayores60, HttpStatus.OK);
     }
 
+    //Cremos los métodos PUT para actualzar los elementos de los Json
     @PutMapping("/ZonaBasicaSalud")
     public ArrayList<ZonaBasicaSalud> actualizar(@RequestBody ArrayList<ZonaBasicaSalud> ZBS) throws ParseException {
+        //Creamos una clase data handling para poder manipular los datos
         DataHanding dataHanding = new DataHanding();
+        //Incluimos los elementos a actualizar
         ArrayList<ZonaBasicaSalud> lista = dataHanding.ActualizarZBS(ZBS);
+        //Creamos la clase que nos permita esccribir en el Json
         EscribirJSON escribirJSON = new EscribirJSON();
+        //EScribimos en el fichero
         escribirJSON.escribirZBS(lista);
         return lista;
     }
