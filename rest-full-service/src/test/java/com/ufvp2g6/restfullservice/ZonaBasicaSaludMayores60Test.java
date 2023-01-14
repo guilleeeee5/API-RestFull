f186bde4a86d39e51c42b85b7a7588c3931bf44f
Jmrx24 <Jmrx24@gmail.com>
2023-01-14 11:22:09 +0100

    Creamos el objeto y lo inicializamos y lo liberamos en los métodos Before y After respectivamente

--- rest-full-service/src/test/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludMayores60Test.java
+++ rest-full-service/src/test/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludMayores60Test.java
@@ -9,14 +9,44 @@ import static org.junit.Assert.*;
 
 public class ZonaBasicaSaludMayores60Test extends TestCase {
 
+    ZonaBasicaSaludMayores60 zB60;
     @Before
     public void setUp() throws Exception {
+        zB60 = new ZonaBasicaSaludMayores60("001", "Abrantes", (float) 182.15,(float) 13.0, "2022/11/29 10:47:00");
     }
 
     @After
     public void tearDown() throws Exception {
+        zB60 = null;
     }
 
+//=====================================================================================
+    //TEST DE MÉTODOS GET
+//=====================================================================================
+    @Test
+    public void testGetCodigo_geometria() {
+    }
+
+    @Test
+    public void testGetZona_basica_salud() {
+    }
+
+    @Test
+    public void testGetTasa_incidencia_acumulada_P60mas_ultimos_14dias() {
+    }
+
+    @Test
+    public void testGetCasos_confirmados_P60mas_ultimos_14dias() {
+    }
+
+    @Test
+    public void testGetFecha_informe() {
+    }
+
+
+//=====================================================================================
+    //TEST DE MÉTODOS SET
+//=====================================================================================
     @Test
     public void testSetCodigo_geometria() {
     }
@@ -37,23 +67,5 @@ public class ZonaBasicaSaludMayores60Test extends TestCase {
     public void testSetFecha_informe() {
     }
 
-    @Test
-    public void testGetCodigo_geometria() {
-    }
 
-    @Test
-    public void testGetZona_basica_salud() {
-    }
-
-    @Test
-    public void testGetTasa_incidencia_acumulada_P60mas_ultimos_14dias() {
-    }
-
-    @Test
-    public void testGetCasos_confirmados_P60mas_ultimos_14dias() {
-    }
-
-    @Test
-    public void testGetFecha_informe() {
-    }
 }
