3f4a2539f7392fb4d039800a64abca5ccc1277fa
Jmrx24 <Jmrx24@gmail.com>
2023-01-14 11:15:35 +0100

    Creacion inicial de la clase test ZonaBasicaSaludMayores60Test y su configuracion inicial

---
+++ rest-full-service/src/test/java/com/ufvp2g6/restfullservice/ZonaBasicaSaludMayores60Test.java
@@ -0,0 +1,59 @@
+package com.ufvp2g6.restfullservice;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZonaBasicaSaludMayores60Test extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSetCodigo_geometria() {
+    }
+
+    @Test
+    public void testSetZona_basica_salud() {
+    }
+
+    @Test
+    public void testSetTasa_incidencia_acumulada_P60mas_ultimos_14dias() {
+    }
+
+    @Test
+    public void testSetCasos_confirmados_P60mas_ultimos_14dias() {
+    }
+
+    @Test
+    public void testSetFecha_informe() {
+    }
+
+    @Test
+    public void testGetCodigo_geometria() {
+    }
+
+    @Test
+    public void testGetZona_basica_salud() {
+    }
+
+    @Test
+    public void testGetTasa_incidencia_acumulada_P60mas_ultimos_14dias() {
+    }
+
+    @Test
+    public void testGetCasos_confirmados_P60mas_ultimos_14dias() {
+    }
+
+    @Test
+    public void testGetFecha_informe() {
+    }
+}
